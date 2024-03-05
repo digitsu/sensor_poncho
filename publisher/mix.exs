27a76ca0429cc1026d058e56b2d972fcbaf9ca12
digitsu <digitsu@gmail.com>
2024-03-05 15:50:12 +0900

    bsv dep

--- publisher/mix.exs
+++ publisher/mix.exs
@@ -22,7 +22,8 @@ defmodule Publisher.MixProject do
   defp deps do
     [
       {:finch, "~> 0.15.0"},
-      {:jason, "~> 1.2.2"}
+      {:jason, "~> 1.2.2"},
+      {:bsv, "~> 2.1.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
